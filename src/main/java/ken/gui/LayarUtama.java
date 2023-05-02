812b4acf076702d397ef724194bce782c1887cac
arieljovananda88 <jovanandaa@gmail.com>
2023-05-02 13:42:45 +0700

    refactor: put LayarUtama.java in src/ken/gui

--- src/main/java/ken/gui/LayarUtama.java
+++ src/main/java/ken/gui/LayarUtama.java
@@ -1,3 +1,5 @@
+package ken.gui;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
--- src/main/java/ken/gui/Main.java
+++ src/main/java/ken/gui/Main.java
@@ -1,3 +1,5 @@
+package ken.gui;
+
 public class Main {
     public static void main(String[] args) {
         new LayarUtama();
