c2cb768ce516d8f2ef9b919a1c54ad78de8deffa
arieljovananda88 <jovanandaa@gmail.com>
2023-05-04 19:53:37 +0700

    feat: setup Kasir.java

---
+++ src/main/java/ken/gui/Kasir.java
@@ -0,0 +1,19 @@
+package ken.gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Kasir extends JPanel {
+    private JPanel inventory;
+    private JPanel cart;
+    Kasir(){
+        this.setSize(500,500);
+        this.setBackground(new Color(0x2C3333));
+        this.setLayout(null);
+        this.setBounds(0,0,500,500);
+    }
+
+    public void makePanelKasir(){
+
+    }
+}
