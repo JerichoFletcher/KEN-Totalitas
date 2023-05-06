47f76ae5d85aa8ea0ce2048d7d8cc66a79228d30
arieljovananda88 <jovanandaa@gmail.com>
2023-05-06 18:53:04 +0700

    fix: inventory shows up on menu bar

--- src/main/java/ken/gui/Panels.java
+++ src/main/java/ken/gui/Panels.java
@@ -14,7 +14,7 @@ public class Panels{
         // Tambahin panel builtin di sini
         panels.put("Kasir", Kasir.class);
         panels.put("Members", Members.class);
-        panels.put("Incentory", Inventory.class);
+        panels.put("Inventory", Inventory.class);
         panels.put("History", History.class);
         panels.put("Settings", Setting.class);
     }
