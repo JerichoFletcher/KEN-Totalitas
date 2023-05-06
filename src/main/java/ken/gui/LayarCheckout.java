ab1d7f70aad2e6482537cbe7f421ad7cc21b1a48
maximatey <maximun154@gmail.com>
2023-05-06 14:48:18 +0700

    fix tidy up

--- src/main/java/ken/gui/LayarCheckout.java
+++ src/main/java/ken/gui/LayarCheckout.java
@@ -4,12 +4,15 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 
 public class LayarCheckout extends JPanel implements ActionListener{
     private JPanel panelBarang;
     private JButton fixBill;
     private JComboBox pilihMember;
+    private JTextField inputField;
     LayarCheckout(){
         super();
         this.setVisible(true);
@@ -24,17 +27,17 @@ public class LayarCheckout extends JPanel implements ActionListener{
         JLabel totalPrice = new JLabel("Total: Rp." + 1000);
         checkoutText.setFont(new Font("Poppins", Font.BOLD,40));
         checkoutText.setForeground(Color.white);
-        checkoutText.setBounds(850,50,500,100);
+        checkoutText.setBounds(550,30,500,100);
         totalPrice.setFont(new Font("Poppins", Font.BOLD,30));
         totalPrice.setForeground(Color.white);
-        totalPrice.setBounds(600,750,500,100);
+        totalPrice.setBounds(35,480,500,100);
         this.add(checkoutText);
         this.add(totalPrice);
         panelBarang = new JPanel();
         panelBarang.setBackground(new Color(0x2C3333));
         panelBarang.setLayout(new BoxLayout(panelBarang, BoxLayout.Y_AXIS));
         panelBarang.setBorder(BorderFactory.createEmptyBorder());
-        for (int i = 1; i <= 10; i++) {
+        for (int i = 1; i <= 20; i++) {
             JLabel item = new JLabel("Label " + i);
             item.setForeground(Color.white);
             item.setFont(new Font("Poppins", Font.BOLD,20));
@@ -42,27 +45,54 @@ public class LayarCheckout extends JPanel implements ActionListener{
         }
         JScrollPane scrollPane = new JScrollPane(panelBarang);
         scrollPane.setBackground(new Color(0, 0, 0, 0));
-        scrollPane.setBounds(600, 150, 700, 600);
+        scrollPane.setBounds(500, 150, 280, 350);
         scrollPane.setBorder(BorderFactory.createEmptyBorder());
         String[] tipeMember = new String[]{"", "jovan", "farhan", "shidqi", "alek", "jericho"};
         pilihMember = new JComboBox(tipeMember);
         pilihMember.setBackground(new Color(0xD9D9D9));
-        pilihMember.setBounds(700,850,500,50);
+        pilihMember.setBounds(920,400,250,50);
         pilihMember.setFont(new Font("Poppins", Font.BOLD,20));
         pilihMember.setForeground(new Color(0x395B64));
         pilihMember.setFocusable(false);
         this.add(pilihMember);
+
+        inputField = new JTextField();
+        inputField.setBounds(920, 400, 250, 50);
+        inputField.setFont(new Font("Poppins", Font.PLAIN, 20));
+        inputField.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                String enteredText = inputField.getText().toLowerCase();
+                DefaultComboBoxModel<String> model = new DefaultComboBoxModel<>();
+                for (String member : tipeMember) {
+                    if (member.toLowerCase().startsWith(enteredText)) {
+                        model.addElement(member);
+                    }
+                }
+                pilihMember.setModel(model);
+                pilihMember.setPopupVisible(model.getSize() > 0);
+            }
+        });
+        this.add(inputField);
         this.add(scrollPane);
         fixBill = new JButton();
         fixBill.addActionListener(this);
         fixBill.setFocusable(false);
         fixBill.setContentAreaFilled( false );
         fixBill.setText("FIX BILL");
-        fixBill.setFont(new Font("Poppins", Font.BOLD,60));
+        fixBill.setFont(new Font("Poppins", Font.BOLD,40));
         fixBill.setBackground(new Color(0, 0, 0, 0));
         fixBill.setForeground(Color.white);
         fixBill.setBorder(BorderFactory.createEmptyBorder());
-        fixBill.setBounds(1500,900,400,200);
+        fixBill.setBounds(920,460,250,75);
         this.add(fixBill);
 
     }
