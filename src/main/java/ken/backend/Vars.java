9c673e046035915efd281585361278dd3b95f131
farhan <mohdfarhanfahrezy@gmail.com>
2023-05-07 20:18:57 +0700

    move db directory out of resource

--- src/main/java/ken/backend/Vars.java
+++ src/main/java/ken/backend/Vars.java
@@ -2,4 +2,6 @@ package ken.backend;
 
 public class Vars{
     public static final String DEFAULT_NAMESPACE = "ken";
+    public static String path = "./db/";
+    public static String extension = "json";
 }
